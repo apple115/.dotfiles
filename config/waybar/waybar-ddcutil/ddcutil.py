6892e834ae2bc9d6d0b6f0d82f2f9aef8902b4a6
apple115 <2567031030@qq.com>
2024-07-04 16:12:35 +0800

    跟新waybar 和 config

--- config/waybar/waybar-ddcutil/ddcutil.py
+++ config/waybar/waybar-ddcutil/ddcutil.py
@@ -4,7 +4,7 @@ import subprocess
 import json
 
 data = {}
-cmd = ["ddcutil", "getvcp", "10", "--bus", "1"]
+cmd = ["ddcutil", "getvcp", "10"]
 value = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf8')
 # output looks like this in my case: VCP code 0x10 (Brightness                    ): current value =    20, max value =   100
 percentage = value.split(":")[1].split(",")[0].split("=")[1].strip(" ")
